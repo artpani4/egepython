06bdde40aa76ed3741c7398304f1240263e7e6fd
Artem <artp80@gmail.com>
2022-03-23 14:46:47 +0300

    install.sh

--- ksi2/ksi/install.sh
+++ ksi2/ksi/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt update
+apt-get install python3-venv
 cd ..
 python3 -m venv myenv
 . myenv/bin/activate
 pip install --upgrade pip
 cd ksi/
 pip install -r requirements.txt
+apt install npm
 
 
